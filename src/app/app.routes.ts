46eb35fc8945d050764c215623cdcfd447ee7f11
Pilo28 <pdefelitto@gmail.com>
2025-07-10 23:09:11 +0200

    setup base routing structure with standalone components, closes #2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/pages/home/home').then(m => m.Home),
+    children: [
+      {
+        path: 'list',
+        loadComponent: () =>
+          import('./features/pages/list/list').then(m => m.List)
+      },
+      {
+        path: 'list/new',
+        loadComponent: () =>
+          import('./features/pages/new/new').then(m => m.New)
+      },
+      {
+        path: 'list/:id/edit',
+        loadComponent: () =>
+          import('./features/pages/edit/edit').then(m => m.Edit)
+      },
+      {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'list'
+  }
+];
