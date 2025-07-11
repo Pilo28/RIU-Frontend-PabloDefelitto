3ae8c7503e2cc67cb207d49bd1d560b3388a2e48
Pilo28 <pdefelitto@gmail.com>
2025-07-11 04:24:40 +0200

    added basic design

--- src/app/features/pages/home/home.ts
+++ src/app/features/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from '../../../shared/components/header/header';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
